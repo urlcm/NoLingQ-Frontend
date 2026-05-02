43c9908cad3702c2b48cbac1af5d7ecbe9dbc88c
url <edwinuriel1122@gmail.com>
2026-05-01 21:41:29 -0500

    Se agregaron los componentes de create-lecture y home-component, ademas de que se actualizo el archivo de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { LectureViewComponent } from './lecture-view/lecture-view.component';
+import { CreateLectureComponent } from './create-lecture/create-lecture.component';
 
 export const routes: Routes = [
         {path: "",component: HomeComponentComponent, },
         {path: "home",component: HomeComponentComponent},
         {path: "lecture",component: LectureViewComponent},
+        {path: "new-lecture",component:CreateLectureComponent}
 ];
