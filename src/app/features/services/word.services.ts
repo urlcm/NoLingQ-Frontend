2b5e97a8f70fd5c25681bab5188ebcacfc012952
url <edwinuriel1122@gmail.com>
2026-05-31 17:48:01 -0500

    Se corrigieron los colores de las etapas de las palabras. Tambien se agregaron los metodos para el servicio de word

--- src/app/features/services/word.services.ts
+++ src/app/features/services/word.services.ts
@@ -9,20 +9,20 @@ import { Observable } from "rxjs";
 export class WordService{
     constructor(private httpClient:HttpClient){}
     
-    main_url = "http://localhost:8081/word-controller/"
-    get_url_by_word = "by-word/";
-    get_url_by_id = "by-id/"
-    save = "save"
+    private main_url = "http://localhost:8081/word-controller/"
+    private get_url_by_word = "by-word/";
+    private get_url_by_id = "by-id/"
+    private save = "save"
 
     getWordByWord(word:string):Observable<Word>{
         return this.httpClient.get<Word>(this.main_url+this.get_url_by_word+word);
     }
 
     getWordById(id:number){
-
+        return this.httpClient.get(this.main_url+this.get_url_by_id+id);
     }
 
-    saveWord(word:Word){
-        
+    saveWord(word:Word):Observable<Word>{
+        return this.httpClient.post<Word>(this.main_url+this.save,word);
     }
 }
