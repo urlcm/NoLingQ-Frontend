8f5bdcc1713a45187aaa91e370ae82f373fc4881
url <edwinuriel1122@gmail.com>
2026-08-18 21:42:13 -0500

    se modifico el inpuTag, ademas de agregarle color y funcionalidad para su uso

--- src/app/definition-view/definition-view.component.ts
+++ src/app/definition-view/definition-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, signal } from '@angular/core';
 import { Word } from '../shared/models/Word';
 import { FormsModule } from '@angular/forms';
 import { WordService } from '../features/services/word.services';
@@ -13,60 +13,92 @@ import { Difficulty, DifficultyLevel } from '../shared/models/Difficulty';
 export class DefinitionViewComponent {
   constructor(
     private wordService: WordService,
-  ){}
+  ) { }
   difficulty: Difficulty = new Difficulty();
 
   @Input() word = new Word;
   isThereDifficulty: boolean;
 
-  saveWord(){
+  tags = signal<string[]>(["Example"]);
+  inputValue = "";
+  placeholderText = input<string>('Type and press Enter...');
+
+  addTag(event: Event) {
+    const keyboardEvent = event as KeyboardEvent;
+
+    keyboardEvent.preventDefault();
+    const value = this.inputValue.trim();
+
+    if (value && !this.tags().includes(value)) {
+      this.tags.update(currentTags => [...currentTags, value]);
+    }
+
+    this.inputValue = "";
+  }
+
+  handleBackspace() {
+    if (this.inputValue === "" && this.tags().length > 0) {
+      this.tags.update(currentTags => {
+        const update = [...currentTags];
+        update.pop();
+        return update;
+      })
+    }
+  }
+
+  removeTag(indexToRemove: number): void {
+    this.tags.update(currentTags => currentTags.filter((_, index) => index !== indexToRemove)
+    )
+  }
+
+  saveWord() {
     this.word.difficulty = this.difficulty;
     this.wordService.saveWord(this.word).subscribe({
-      next:(value) => {
-          Object.assign(this.word, value);
-          this.difficulty = new Difficulty();
+      next: (value) => {
+        Object.assign(this.word, value);
+        this.difficulty = new Difficulty();
       },
-      error: (error:any) => {
-        console.error("Se obtuvo el siguiente error al querer ingresar",error);
+      error: (error: any) => {
+        console.error("Se obtuvo el siguiente error al querer ingresar", error);
       }
     })
   }
 
-  updateWord(){
+  updateWord() {
     this.word.difficulty = this.difficulty;
     this.wordService.updateWord(this.word).subscribe({
-      next:(value) => {
-          Object.assign(this.word, value);
-          this.difficulty = new Difficulty();
-          console.info("Se actualizó la palabra: "+this.word+" con dificultad: "+this.word.difficulty.idDifficulty);
+      next: (value) => {
+        Object.assign(this.word, value);
+        this.difficulty = new Difficulty();
+        console.info("Se actualizó la palabra: " + this.word + " con dificultad: " + this.word.difficulty.idDifficulty);
       },
-      error: (error:any) => {
-        console.error("Se obtuvo el siguiente error al querer actualizar: ",error);
+      error: (error: any) => {
+        console.error("Se obtuvo el siguiente error al querer actualizar: ", error);
       }
     })
   }
 
-  changeStatus(id:number){
+  changeStatus(id: number) {
     this.difficulty.idDifficulty = id;
-    console.info("Se cambia la dificultad a: "+this.difficulty.idDifficulty)
+    console.info("Se cambia la dificultad a: " + this.difficulty.idDifficulty)
   }
 
-  saveDifficulty(){
+  saveDifficulty() {
     if (!this.difficulty.idDifficulty) {
-        console.warn("Debes seleccionar una dificultad antes de guardar");
-        this.isThereDifficulty = true;
-        return;
+      console.warn("Debes seleccionar una dificultad antes de guardar");
+      this.isThereDifficulty = true;
+      return;
     }
 
     this.word.difficulty = this.difficulty;
     this.word.word = this.word.word.toLowerCase();
 
     if (!this.word.idWord || this.word.idWord === -1) {
-        console.log("Entra en saveWord");
-        this.saveWord();
+      console.log("Entra en saveWord");
+      this.saveWord();
     } else {
-        console.log("Entra en update");
-        this.updateWord();
+      console.log("Entra en update");
+      this.updateWord();
     }
 
     this.isThereDifficulty = false;
--- src/app/lecture-component/lecture-component.component.ts
+++ src/app/lecture-component/lecture-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Word } from '../shared/models/Word';
 import { formatWord } from "../shared/utils/formatword.utils";
