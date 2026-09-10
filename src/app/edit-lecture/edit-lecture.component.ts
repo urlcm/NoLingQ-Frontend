cd6113ef195ecfe2de385a5a03f75c95df686448
url <edwinuriel1122@gmail.com>
2026-09-10 16:31:55 -0500

    Se crea nuevo componente para editar lecturas completamente funcional en el cual se comenta el atributo de dictionary

---
+++ src/app/edit-lecture/edit-lecture.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { LectureService } from '../shared/services/Lecture.service';
+import { Lecture } from '../shared/models/Lecture';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-lecture',
+  imports: [FormsModule],
+  templateUrl: './edit-lecture.component.html',
+  styleUrl: './edit-lecture.component.css'
+})
+export class EditLectureComponent implements OnInit {
+
+  constructor(
+    private lectureService: LectureService,
+    private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    let id: number = +this.route.snapshot.paramMap.get("id");
+    this.getLecture(id);
+  }
+
+  name_lecture: string = "";
+  url_media: string = "";
+  url_dictionary: string = "";
+  url_text: string = "";
+
+  lecture: Lecture;
+
+  save() {
+    this.lecture.name = this.name_lecture;
+    this.lecture.sourceLecture.urlSource = this.url_text;
+    this.lecture.sourceMedia.urlSource = this.url_media;
+
+    this.lectureService.updateLecture(this.lecture).subscribe({
+      next: (data) => {
+        console.log("Data modificada", data);
+      },
+      error: (error) => {
+        console.error("se dio el siguiente error al querer modificar", error);
+      }
+    });
+  }
+
+  getLecture(id: number) {
+    this.lectureService.getLectureById(id).subscribe((data: Lecture) => {
+      this.name_lecture = data.name;
+      this.url_text = data.sourceLecture.urlSource;
+      this.url_media = data.sourceMedia.urlSource;
+      this.lecture = data;
+    })
+  }
+
+
+}
