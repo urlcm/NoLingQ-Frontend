1a00da3d538901f04b6dc0ca34f10f970c07c708
url <edwinuriel1122@gmail.com>
2026-09-10 16:29:43 -0500

    Se agrego la ruta de edit-lecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { LectureViewComponent } from './lecture-view/lecture-view.component';
 import { CreateLectureComponent } from './create-lecture/create-lecture.component';
+import { EditLectureComponent } from './edit-lecture/edit-lecture.component';
 
 export const routes: Routes = [
         {path: "",component: HomeComponentComponent, },
         {path: "home",component: HomeComponentComponent},
         {path: "lecture/:id",component: LectureViewComponent},
-        {path: "new-lecture",component:CreateLectureComponent}
+        {path: "new-lecture",component:CreateLectureComponent},
+        {path: "edit-lecture/:id",component:EditLectureComponent}
 ];
